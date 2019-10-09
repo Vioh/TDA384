c5aef73c4d7baf9831868e8454f1516f6e579ce8
Hai Dinh <dinhsyhai@gmail.com>
2019-10-09 17:16:19 +0200

    Implement CCHAT lab (original attempt)
    In this solution, we have a single process taking care of the server,
    and thus if the server process is killed, all communications between
    clients will also fail. This leads to failure of some of the test
    cases, and thus we must come up with a different solution.

--- cchat/client.erl
+++ cchat/client.erl
@@ -1,6 +1,9 @@
 -module(client).
 -export([handle/2, initial_state/3]).
 
+% TODO: Think about how to reuse the same thing for the 3 functions
+% TODO: Where to insert server_not_reached?
+
 % This record defines the structure of the state of a client.
 % Add whatever other fields you need.
 -record(client_st, {
@@ -18,6 +21,15 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
         server = ServerAtom
     }.
 
+% Send a request to the server, and wait to return a response.
+send(St, Request) ->
+    try genserver:request(St#client_st.server, Request) of
+        Response -> Response
+    catch
+        error:_ -> {error, server_not_reached, "Server not reached!"}
+        % TODO: What about other types (exit, throw)?
+    end.
+
 % handle/2 handles each kind of request from GUI
 % Parameters:
 %   - the current state of the client (St)
@@ -28,24 +40,34 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Join channel
 handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "join not implemented"}, St} ;
+    case send(St, {join, self(), St#client_st.nick, Channel}) of
+        {error, ErrorAtom, ErrorMsg} ->
+            {reply, {error, ErrorAtom, ErrorMsg}, St};
+        ok ->
+            {reply, ok, St}
+        end;
 
 % Leave channel
 handle(St, {leave, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "leave not implemented"}, St} ;
+    case send(St, {leave, self(), Channel}) of
+        {error, ErrorAtom, ErrorMsg} ->
+            {reply, {error, ErrorAtom, ErrorMsg}, St};
+        ok ->
+            {reply, ok, St}
+        end;
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "message sending not implemented"}, St} ;
+    case send(St, {message_send, self(), Channel, Msg}) of
+        {error, ErrorAtom, ErrorMsg} ->
+            {reply, {error, ErrorAtom, ErrorMsg}, St};
+        ok ->
+            {reply, ok, St}
+        end;
 
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
+% TODO
 handle(St, {nick, NewNick}) ->
     {reply, ok, St#client_st{nick = NewNick}} ;
 
@@ -68,5 +90,5 @@ handle(St, quit) ->
     {reply, ok, St} ;
 
 % Catch-all for any unhandled requests
-handle(St, Data) ->
+handle(St, _) ->
     {reply, {error, not_implemented, "Client does not handle this command"}, St} .
--- cchat/server.erl
+++ cchat/server.erl
@@ -1,18 +1,87 @@
 -module(server).
 -export([start/1,stop/1]).
 
+-record(server_st, {
+    c2m, % dictionary mapping every channel to its members (represented by the PIDs)
+    m2n  % dictionary mapping every member (represented by the PID) to its nick name
+}).
+
+initial_state() ->
+    #server_st{
+        c2m = dict:new(),
+        m2n = dict:new()
+    }.
+
+handle(St, {join, ClientPid, ClientNick, Channel}) ->
+    case dict:find(Channel, St#server_st.c2m) of
+        {ok, Members} ->
+            % Channel has been created
+            case lists:member(ClientPid, Members) of
+                true ->
+                    % Client has already joined
+                    {reply, {error, user_already_joined, "User already joined "++Channel}, St};
+                false ->
+                    % Client has not joined
+                    NewC2M = dict:append(Channel, ClientPid, St#server_st.c2m),
+                    NewM2N = dict:store(ClientPid, ClientNick, St#server_st.m2n),
+                    {reply, ok, St#server_st{c2m=NewC2M, m2n=NewM2N}}
+                end;
+        error ->
+            % Channel has not been created
+            NewC2M = dict:store(Channel, [ClientPid], St#server_st.c2m),
+            NewM2N = dict:store(ClientPid, ClientNick, St#server_st.m2n),
+            {reply, ok, St#server_st{c2m=NewC2M, m2n=NewM2N}}
+        end;
+
+handle(St, {leave, ClientPid, Channel}) ->
+    case dict:find(Channel, St#server_st.c2m) of
+        {ok, Members} ->
+            % Channel has been created
+            case lists:member(ClientPid, Members) of
+                true ->
+                    % Client has already joined
+                    NewC2M = dict:store(Channel, lists:delete(ClientPid, Members), St#server_st.c2m),
+                    {reply, ok, St#server_st{c2m=NewC2M}};
+                false ->
+                    % Client has not joined
+                    {reply, {error, user_not_joined, "User has not joined "++Channel}, St}
+                end;
+        error ->
+            % Channel has not been created
+            {reply, {error, user_not_joined, "User has not joined "++Channel}, St}
+        end;
+
+handle(St, {message_send, ClientPid, Channel, Msg}) ->
+    case dict:find(Channel, St#server_st.c2m) of
+        {ok, Members} ->
+            % Channel has been created
+            case lists:member(ClientPid, Members) of
+                true ->
+                    % Client has already joined
+                    ClientNick = dict:fetch(ClientPid, St#server_st.m2n),
+                    OtherMembers = lists:delete(ClientPid, Members),
+                    Data = {request, self(), make_ref(), {message_receive, Channel, ClientNick, Msg}},
+                    lists:foreach((fun(Member) -> Member ! Data end), OtherMembers),
+                    {reply, ok, St};
+                false ->
+                    % Client has not joined
+                    {reply, {error, user_not_joined, "User has not joined "++Channel}, St}
+                end;
+        error ->
+            % Channel has not been created
+            {reply, {error, user_not_joined, "User has not joined "++Channel}, St}
+        end;
+
+% TODO: Think about the atom here
+handle(St, _) ->
+    {reply, {error, unknown_request, "Unknown request"}, St}.
+
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    not_implemented.
+    genserver:start(ServerAtom, initial_state(), fun handle/2).
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+    genserver:stop(ServerAtom).
